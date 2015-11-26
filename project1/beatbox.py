9bab92a810233b2486536f8d6daf785af4f5158f
Justyna Jankowiak <just.jankowiak@gmail.com>
2015-11-26 20:39:41 +0100

    done

--- project1/beatbox.py
+++ project1/beatbox.py
@@ -8,16 +8,19 @@ import zipfile
 from os import path
 from read_defs import read_defs
 
+#main program
 if __name__=="__main__":
     import sys
     import os
     path0 = os.getcwd()
     folder = sys.argv[1]
+    #uncompressed file
     if path.isdir(path.join(path0, folder)):
     	defs = read_defs(folder)
     	bpm = defs['bpm']
     	song = ms.create_song(bpm, folder)
     	wav.write(path.join(folder, folder + '.wav'), 44100, song)
+    #compressed file
     elif folder[-3:] == "zip":
     	zip = zipfile.ZipFile(folder)
     	dirpath = tempfile.mkdtemp()
--- project1/modul_to_create_column.py
+++ project1/modul_to_create_column.py
@@ -7,21 +7,21 @@ def read_sample(column_of_track, length_of_column, folder, next_sample):
 	Function creates array which represents one column in track file which consists of two signs.
 	"""
 	# check the number of sample (only one type in one column)
-    num_of_sample = list(set(column_of_track).difference(['--']))[0]
-    sample = wav.read(path.join(folder, 'sample' + num_of_sample + '.wav'))
-    # number of bits in the sample
-    n = sample[1].shape[0]
-    # if length of sample is bigger than number of sounds (?) per bit we add more columns at the end of array
-    if n > next_sample:
-        how_many_to_add = n - next_sample
-    else:
-    	how_many_to_add = 0
-    column = np.zeros((length_of_column + how_many_to_add, 2))
-    # concatenate sample and 'empty sounds' in order
-    for i, j in enumerate(column_of_track):
-        if j != '--':
-            column[i * next_sample : i * next_sample + n, :] += sample[1]
-    return column
+	num_of_sample = list(set(column_of_track).difference(['--']))[0]
+	sample = wav.read(path.join(folder, 'sample' + num_of_sample + '.wav'))
+	# number of bits in the sample
+	n = sample[1].shape[0]
+	# if length of sample is bigger than number of sounds (?) per bit we add more columns at the end of array
+	if n > next_sample:
+		how_many_to_add = n - next_sample
+	else:
+		how_many_to_add = 0
+	column = np.zeros((length_of_column + how_many_to_add, 2))
+	# concatenate sample and 'empty sounds' in order
+	for i, j in enumerate(column_of_track):
+	    if j != '--':
+	        column[i * next_sample : i * next_sample + n, :] += sample[1]
+	return column
 
 def create_frequency(note_string):
 	"""
@@ -56,6 +56,3 @@ def create_note(column_of_track, length_of_column, folder, next_sample):
 			sample = np.transpose(np.vstack((y, y)))
 			column[i * next_sample : i * next_sample + next_sample, :] += sample
 	return column
-
-	
-
